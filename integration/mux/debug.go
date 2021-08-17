968fd2c48d1f3981f98b9cf35f6c546cd121909a
Kazuki Sawada <kazuki@6715.jp>
2021-08-17 15:29:37 +0900

    mv handler def

--- integration/mux/debug.go
+++ integration/mux/debug.go
@@ -3,11 +3,7 @@
 package mux
 
 import (
-	"net/http/pprof"
-
-	"github.com/felixge/fgprof"
 	"github.com/gorilla/mux"
-	"github.com/kaz/pprotein/internal/tail"
 )
 
 func Integrate(r *mux.Router) {
@@ -16,16 +12,7 @@ func Integrate(r *mux.Router) {
 }
 
 func EnableDebugHandler(r *mux.Router) {
-	r.Handle("/debug/log/nginx", tail.NewTailHandler("/var/log/nginx/access.log"))
-	r.Handle("/debug/log/mysql", tail.NewTailHandler("/var/log/mysql/mysql-slow.log"))
-
-	r.Handle("/debug/fgprof", fgprof.Handler())
-
-	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
-	r.HandleFunc("/debug/pprof/{h:.*}", pprof.Index)
+	registerDebugHandlers(r)
 }
 
 func EnableDebugMode(r *mux.Router) {
--- integration/mux/mux.go
+++ integration/mux/mux.go
@@ -2,12 +2,29 @@ package mux
 
 import (
 	"net/http"
+	"net/http/pprof"
 
+	"github.com/felixge/fgprof"
 	"github.com/gorilla/mux"
+	"github.com/kaz/pprotein/internal/tail"
 )
 
 func NewDebugHandler() http.Handler {
 	r := mux.NewRouter()
 	EnableDebugHandler(r)
 	return r
+}
+
+func registerDebugHandlers(r *mux.Router) {
+	r.Handle("/debug/log/test", tail.NewTailHandler("./test"))
+	r.Handle("/debug/log/nginx", tail.NewTailHandler("/var/log/nginx/access.log"))
+	r.Handle("/debug/log/mysql", tail.NewTailHandler("/var/log/mysql/mysql-slow.log"))
+
+	r.Handle("/debug/fgprof", fgprof.Handler())
+
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	r.HandleFunc("/debug/pprof/{h:.*}", pprof.Index)
 }
