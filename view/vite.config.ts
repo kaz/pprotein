77a927240672c48253ff8d374417857030945939
Kazuki Sawada <kazuki@6715.jp>
2021-09-12 23:32:49 +0900

    fix type error

--- view/vite.config.ts
+++ view/vite.config.ts
@@ -1,11 +1,11 @@
 import vue from "@vitejs/plugin-vue";
-import { UserConfig } from "vite";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
       "/api": "http://localhost:9000",
     },
   },
-} as UserConfig;
+});
