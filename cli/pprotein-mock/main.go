db440d7008ec147244fdd7258f4442926589e097
Kazuki Sawada <kazuki@6715.jp>
2021-09-17 23:19:27 +0900

    mock: fix connection leak

--- cli/pprotein-mock/main.go
+++ cli/pprotein-mock/main.go
@@ -20,15 +20,19 @@ var (
 )
 
 func handle(c echo.Context) error {
-	time.Sleep(time.Duration(rand.Int63n(4096)) * time.Millisecond)
+	time.Sleep(time.Duration(rand.Int63n(2048)) * time.Millisecond)
 
 	if _, err := db.Exec("INSERT INTO mock.mock VALUES (?, NOW())", c.Param("id")); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
-	if err := db.QueryRow("SELECT * FROM mock.mock WHERE id = ?", c.Param("id")).Err(); err != nil {
+
+	result := ""
+	row := db.QueryRow("SELECT * FROM mock.mock WHERE id = ?", c.Param("id"))
+	if err := row.Scan(&result); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err)
 	}
-	return c.String(http.StatusOK, "ok")
+
+	return c.String(http.StatusOK, result)
 }
 
 func start() error {
@@ -37,8 +41,6 @@ func start() error {
 	if err != nil {
 		return err
 	}
-	defer db.Close()
-
 	if _, err := db.Exec("CREATE DATABASE IF NOT EXISTS mock"); err != nil {
 		return err
 	}
@@ -59,13 +61,13 @@ func start() error {
 }
 
 func startRequest() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 8; i++ {
 		go func() {
 			for {
 				if _, err := http.Get(fmt.Sprintf("http://%s/api/mock/%s", os.Getenv("REQUEST_HOST"), strconv.FormatInt(time.Now().UnixNano(), 36))); err != nil {
 					log.Printf("request failed: %v", err)
 				}
-				time.Sleep(time.Duration(rand.Int63n(4096)) * time.Millisecond)
+				time.Sleep(time.Duration(rand.Int63n(2048)) * time.Millisecond)
 			}
 		}()
 	}
